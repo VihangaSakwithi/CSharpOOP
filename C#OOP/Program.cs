32b76113cdb21650e005c0f5a5701e6ddb947100
vsakw <vsakw@ASPIRE>
2023-06-14 12:04:41 +0530

    Add project files.

---
+++ C#OOP/Program.cs
@@ -0,0 +1,11 @@
+﻿//Class and Objects
+class Fruit
+{
+    String Fruit1 = "Apple";
+
+    static void Main(string[] args)
+    {
+        Fruit NewObj = new Fruit();
+        Console.WriteLine(NewObj.Fruit1);
+    }
+}
